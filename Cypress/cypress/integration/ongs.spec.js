7232b55769001ca033f7ae1806ec6761ca854fec
Aline Mendes <80456251+AlineMend@users.noreply.github.com>
2022-03-02 07:52:54 -0300

    Alterações (02/03/22)

---
+++ Cypress/cypress/integration/ongs.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Ongs', () => {
+    it('Deve poder realizar um cadastro', () => {
+        
+    });
+    it('Deve poder realizar um login no sistema', () => {
+        
+    });
+})
