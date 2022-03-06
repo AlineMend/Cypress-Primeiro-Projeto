0f5c75106a0f94ed7d41c022d3cebc9eb61627b6
Aline Mendes <80456251+AlineMend@users.noreply.github.com>
2022-03-06 08:56:45 -0300

    Alterações (06/03/22)

--- be-the-hero-master/frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-//   baseURL: 'http://localhost:3333',
-  baseURL: 'https://betheheroapi4.herokuapp.com/',
-})
-
-export default api;
--- be-the-hero-master/mobile/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: '<addYourOwnUrl>'
-});
-
-export default api;
