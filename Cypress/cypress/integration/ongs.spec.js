76c577dbf4e4d44a714054271f9c096f768ea239
Aline Mendes <80456251+AlineMend@users.noreply.github.com>
2022-03-06 10:59:43 -0300

    Alterações (06/03/22)

--- Cypress/cypress/integration/ongs.spec.js
+++ Cypress/cypress/integration/ongs.spec.js
@@ -2,7 +2,13 @@
 
 describe('Ongs', () => {
     it('Deve poder realizar um cadastro', () => {
-        
+        cy.visit( 'http://localhost:3000/register');
+        cy.get('[data-cy=name]').type('Dogs queridos');
+        cy.get('[data-cy=email]').type('Dogs@gmail.com');
+        cy.get('[data-cy=whatsapp]').type('51999999999');
+        cy.get('[data-cy=city]').type('Porto Alegre');
+        cy.get('[data-cy=uf]').type('RS');
+        cy.get('[data-cy=submit]').click();
     });
     it('Deve poder realizar um login no sistema', () => {
         
